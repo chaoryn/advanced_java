e903d12ad1f748cf390b5383ce35f0ea265eea9c
Nando <141431859+chaoryn@users.noreply.github.com>
2023-11-05 12:05:07 +0530

    Update employee_HRManager.java

--- java_inheritance/employee_HRManager.java
+++ java_inheritance/employee_HRManager.java
@@ -1,3 +1,5 @@
+// 4. Write a Java program to create a class called Employee with methods called work() and getSalary(). Create a subclass called HRManager that overrides the work() method and adds a new method called addEmployee().
+
 public class Main {
     public static void main(String[] args) {
         Employee emp = new Employee(100000);
