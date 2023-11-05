adfbe4d919e10791ebc84f1c31854889b6eb0ced
Nando <141431859+chaoryn@users.noreply.github.com>
2023-11-05 12:05:37 +0530

    Update area_getShape.java

--- java_inheritance/area_getShape.java
+++ java_inheritance/area_getShape.java
@@ -1,4 +1,6 @@
 //Write a Java program to create a class called Shape with a method called getArea(). Create a subclass called Rectangle that overrides the getArea() method to calculate the area of a rectangle.
+
+
 public class Main {
     public static void main(String[] args){
         Rectangle rect1 = new Rectangle(1.0, 2.0); //don't forget to include parameters!
